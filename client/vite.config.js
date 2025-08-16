8886abe814243eda91e3c735d71abc7199e60d5d
Manek Vedant Umesh <u23ec108@eced.svnit.ac.in>
2025-08-16 21:29:13 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://react-jobs-backend-dux4.onrender.com",
         changeOrigin: true
       },
     },
